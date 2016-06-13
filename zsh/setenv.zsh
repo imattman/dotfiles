e831054aaa9886de07f1d07f8e8f41f661fbd1ad
Matt Phillips <code@mattman.dev>
2016-06-12 21:10:36 -0700

    - Updated aliases with docker - Added Go config - Updated CDPATH with GOPATH + GitHub

--- zsh/setenv.zsh
+++ zsh/setenv.zsh
@@ -3,3 +3,7 @@
 # base setenv.sh for non-standard env vars that don't
 # really belong in .zprofile
 
+export GOPATH=$HOME/workspace/gocode
+export PATH=$PATH:$GOPATH/bin
+export CDPATH=workspace:$GOPATH/src/github.com/imattman
+
