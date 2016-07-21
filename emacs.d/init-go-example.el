99edd8af99eb3a533582191324035703938fc1a8
Matt Phillips <mattman@mattcave.com>
2016-07-21 16:18:53 -0700

    Added golang emacs example plucked from blog

---
+++ emacs.d/init-go-example.el
@@ -0,0 +1,87 @@
+;; example from:
+;;   http://arenzana.org/2015/Emacs-for-Go/
+
+;; Load package-install sources
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   '("melpa" . "http://melpa.org/packages/")
+   t)
+  (package-initialize))
+
+(defvar my-packages
+  '(;;;; Go shit
+    go-mode
+    go-eldoc
+    go-autocomplete
+
+        ;;;;;; Markdown
+    markdown-mode
+
+        ;;;;;; Javascript
+    json-mode
+        ;;;;;; Env
+    project-explorer
+    smooth-scroll
+    buffer-move
+    window-number)
+  "My packages!")
+
+;; fetch the list of packages available
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; install the missing packages
+(dolist (package my-packages)
+  (unless (package-installed-p package)
+    (package-install package)))
+    
+;;Load Go-specific language syntax
+(defun go-mode-setup ()
+  (go-eldoc-setup))
+
+(add-hook 'go-mode-hook 'go-mode-setup)
+
+;;Format before saving
+(defun go-mode-setup ()
+  (go-eldoc-setup)
+  (add-hook 'before-save-hook 'gofmt-before-save))
+(add-hook 'go-mode-hook 'go-mode-setup)
+
+;;Goimports
+(defun go-mode-setup ()
+  (go-eldoc-setup)
+  (setq gofmt-command "goimports")
+  (add-hook 'before-save-hook 'gofmt-before-save))
+(add-hook 'go-mode-hook 'go-mode-setup)
+
+;;Godef, shows function definition when calling godef-jump
+(defun go-mode-setup ()
+  (go-eldoc-setup)
+  (setq gofmt-command "goimports")
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  (local-set-key (kbd "M-.") 'godef-jump))
+(add-hook 'go-mode-hook 'go-mode-setup)
+
+;;Custom Compile Command
+(defun go-mode-setup ()
+  (setq compile-command "go build -v && go test -v && go vet && golint && errcheck")
+  (define-key (current-local-map) "\C-c\C-c" 'compile)
+  (go-eldoc-setup)
+  (setq gofmt-command "goimports")
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  (local-set-key (kbd "M-.") 'godef-jump))
+(add-hook 'go-mode-hook 'go-mode-setup)
+
+;;Load auto-complete
+(ac-config-default)
+(require 'auto-complete-config)
+(require 'go-autocomplete)
+
+(add-to-list 'load-path (concat (getenv "GOPATH")  "/src/github.com/golang/lint/misc/emacs"))
+(require 'golint)
+
+;;Project Explorer
+(require 'project-explorer)
+(global-set-key (kbd "M-e") 'project-explorer-toggle)
