8bab9225b8e817cca318fcb82df6aebd59d6640e
Matt Phillips <mattman@mattcave.com>
2016-07-26 17:37:43 -0700

    unified shell config under 'shell/' path

--- zsh/setenv.zsh
+++
@@ -1,9 +0,0 @@
-# mattman  .dotfiles/zsh/setenv.sh
-
-# base setenv.sh for non-standard env vars that don't
-# really belong in .zprofile
-
-export GOPATH=$HOME/workspace/gocode
-export PATH=$PATH:$GOPATH/bin
-export CDPATH=workspace:$GOPATH/src/github.com/imattman
-
