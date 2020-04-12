333dbfc7dae879f814b79047e5862f98894b297f
Matt Phillips <code@mattman.dev>
2020-04-12 09:38:11 -0700

    Updated installer to handle GO11MODULE installs
    - process 2 different package list files
    - GOMOD is determined from file name for now
    - cleaned up DEBUG handling

--- golang/install.sh
+++ golang/install.sh
@@ -3,39 +3,64 @@
 # Install common Go utils & libraries
 
 # fail early
-set -euo pipefail
-#IFS=$'\n\t'
+set -eou pipefail
 
-DEBUG=${DEBUG:-}
-if [[ -n "$DEBUG" ]]; then
+if [[ -n "${DEBUG:=}" ]]; then
   set -x
 fi
 
-pkg_file='golang-pkg-list.txt'
-pkg_file="$(dirname $0)/$pkg_file"
+THIS_SCRIPT=$(basename "$0")
+THIS_DIR=$(dirname "$0")
+BASE_DIR=$(cd "$THIS_DIR" && pwd)
+
+PKG_FILE='golang-pkg-list.txt'
+PKG_FILE="$THIS_DIR/$PKG_FILE"
+
+GOMOD_FILE='golang-gomod-list.txt'
+GOMOD_FILE="$THIS_DIR/$GOMOD_FILE"
+
 # allow package file to be specified as argument
-pkg_file="${1:-$pkg_file}"
+PKG_FILE="${1:-$PKG_FILE}"
+
+GOMOD_PREFIX='env GO111MODULE=on'
+CMD_PREFIX=''
+
+
+install_from_file() {
+  local= pkg_file="$1"
+
+  if [[ $(echo $pkg_file | grep -- '-gomod-') ]] ; then
+    echo Installing Go module packages from $pkg_file
+    CMD_PREFIX="$CMD_PREFIX $GOMOD_PREFIX"
+  else
+    echo Installing classic packages from $pkg_file
+    if [[ -z "$GOPATH" ]] ; then
+      echo '$GOPATH is not set'
+      exit 1
+    fi
+  fi
+
+  sed -e 's/\s*#.*//' -e '/^$/d' < "$pkg_file" | \
+  while read pkg; do
+    echo $CMD_PREFIX go get -v -u $pkg
+    $CMD_PREFIX go get -v -u $pkg
+    echo
+  done
+}
+
 
 # Check for go binary and $GOPATH
-if [[ ! "$(which go)" ]] ; then
+if [[ ! "$(command -v go)" ]] ; then
   echo "Go binary not found in path"
   exit 1
 fi
 
-if [[ -z "$GOPATH" ]] ; then
-  echo '$GOPATH is not set'
-  exit 1
+install_from_file $PKG_FILE
+
+if [[ $# -eq 0 ]] ; then
+  install_from_file $GOMOD_FILE
 fi
 
-
-echo Installing packages from $pkg_file
-
-sed -e 's/\s*#.*//' -e '/^$/d' < "$pkg_file" | \
-while read pkg; do
-  echo go get -v -u $pkg
-  go get -v -u $pkg
-  echo
-done
-
 exit 0
+
 
