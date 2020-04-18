27a2f156c75d7469e7b9d2d21480e3acf01a569d
Matt Phillips <code@mattman.dev>
2020-04-18 12:37:10 -0700

    Added snippet directory to VS Code installer
    Updated list of extensions

--- vscode/install.sh
+++ vscode/install.sh
@@ -8,28 +8,53 @@ if [[ -n "${DEBUG:=}" ]]; then
 fi
 
 
-base_dir="$(cd $(dirname $0) && pwd)"
-src_file="settings.json"
+THIS_SCRIPT=$(basename "$0")
+THIS_DIR=$(dirname "$0")
+BASE_DIR=$(cd "$THIS_DIR" && pwd)
 
-dest="$HOME/Library/Application Support/Code/User"
-dest_file="${dest}/${src_file}"
+SRC_CONFIG="settings.json"
+SRC_SNIPPETS="snippets"
 
-if [[ ! -d "$dest" ]] ; then
-  echo "Directory not found: $dest"
+DEST_DIR="$HOME/Library/Application Support/Code/User"
+DEST_CONFIG="${DEST_DIR}/${SRC_CONFIG}"
+DEST_SNIPPETS="${DEST_DIR}/${SRC_SNIPPETS}"
+SRC_CONFIG="${BASE_DIR}/${SRC_CONFIG}"
+SRC_SNIPPETS="${BASE_DIR}/${SRC_SNIPPETS}"
+
+
+if [[ ! -d "$DEST_DIR" ]] ; then
+  echo "Directory not found: $DEST_DIR"
   echo
-  echo Launch VS Code to initialize config paths before running this.
+  echo Launch VS Code to initialize config paths before running this script.
   exit 1
 fi
 
-#echo "copying $src_file to $dest"
-#cp "${base_dir}/${src_file}" "$dest"
 
-if [[ -e "$dest_file" ]]; then
-  dest_backup="${dest_file}.bak"
-  echo "moving old config to $dest_backup"
-  mv "$dest_file" "$dest_backup"
+if [[ -L "$DEST_CONFIG" ]]; then
+  echo "Symlink already exists: $DEST_CONFIG"
+else
+  if [[ -f "$DEST_CONFIG" ]]; then
+    dest_backup="${DEST_CONFIG}.bak"
+    echo "moving old config to $dest_backup"
+    mv "$DEST_CONFIG" "$dest_backup"
+  fi
+
+  echo "linking $SRC_CONFIG to $DEST"
+  ln -s "${SRC_CONFIG}" "$DEST_CONFIG"
 fi
 
-echo "linking $src_file to $dest"
-ln -s "${base_dir}/${src_file}" "$dest_file"
+
+if [[ -L "$DEST_SNIPPETS" ]]; then
+    echo "Symlink already exists: $DEST_SNIPPETS"
+else
+  if [[ -d "$DEST_SNIPPETS" ]]; then
+    dest_backup="${DEST_SNIPPETS}.bak"
+    echo "moving old snippets to $dest_backup"
+    mv "$DEST_SNIPPETS" "$dest_backup"
+  fi
+
+  echo "linking $SRC_SNIPPETS to $DEST_SNIPPETS"
+  ln -s "$SRC_SNIPPETS" "$DEST_SNIPPETS"
+fi
+
 
