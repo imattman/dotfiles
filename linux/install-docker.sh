1660b5a171e1aff48562bb88e2089619b11e4919
Matt Phillips <code@mattman.dev>
2020-12-08 09:12:00 -0800

    Updated linux packages

---
+++ linux/install-docker.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+THIS_SCRIPT=$(basename "$0")
+THIS_DIR=$(dirname "$0")
+BASE_DIR=$(cd "$THIS_DIR" && pwd)
+
+sudo apt update
+sudo apt install docker.io
+
+sudo usermod -aG docker $USER
+sudo systemctl enable --now docker
+
+echo "Log out for the group change to take effect"
+
---
+++ linux/install-openssh-server.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+THIS_SCRIPT=$(basename "$0")
+THIS_DIR=$(dirname "$0")
+BASE_DIR=$(cd "$THIS_DIR" && pwd)
+
+sudo apt update
+sudo apt install openssh-server
+
+# verify it started
+sudo systemctl status ssh
+
+
