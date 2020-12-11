b6c7eab94e79e2f9769cfc2061686670c411eac3
Matt Phillips <code@mattman.dev>
2020-12-10 16:42:00 -0800

    moved linux gnome install to separate script

---
+++ linux/install-gnome-updates.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+THIS_SCRIPT=$(basename "$0")
+THIS_DIR=$(dirname "$0")
+BASE_DIR=$(cd "$THIS_DIR" && pwd)
+
+
+sudo apt install gnome-tweaks \
+  gnome-sushi \
+  gnome-user-share 
+
+
+
