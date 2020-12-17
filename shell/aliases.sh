9403c97859787ada0abec35f51501a797a60d97d
Matt Phillips <code@mattman.dev>
2020-12-17 15:43:49 -0800

    Updated shell aliases for python venv

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -37,8 +37,8 @@ alias dcomp='docker-compose'
 alias dmenv='eval $(docker-machine env)'
 
 # python
-alias venv='python3 -m venv venv && source venv/bin/activate && python3 -m pip install --upgrade pip && rehash'
-alias venv-site='python3 -m venv venv --system-site-packages && source venv/bin/activate && python3 -m pip install --upgrade pip setuptools wheels && rehash'
+alias venv='python3 -m venv venv && source venv/bin/activate && python3 -m pip install --upgrade pip setuptools wheel && rehash'
+alias venv-site='python3 -m venv venv --system-site-packages && source venv/bin/activate && python3 -m pip install --upgrade pip setuptools wheel && rehash'
 alias py_venv=venv
 alias py_venv_site=venv-site
 alias activate='py_activate_virtualenv'
