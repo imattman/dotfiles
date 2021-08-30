72f6634f3c17d94d55902c6d98e541e25f183fe8
Matt Phillips <code@mattman.dev>
2021-08-30 13:11:13 -0700

    updated linux setup

--- linux/install-fonts.sh
+++ linux/install-fonts.sh
@@ -6,10 +6,8 @@ BASE_DIR=$(cd "$THIS_DIR" && pwd)
 
 
 sudo apt install -y \
-  gconf2 \
-  gnome-tweaks \
-  gnome-sushi \
-  gnome-user-share 
-
+  fonts-firacode \
+  fonts-hack \
+  fonts-inconsolata
 
 
