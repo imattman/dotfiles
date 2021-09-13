38874309f21f233fc49732108b3c5df18a117108
Matt Phillips <code@mattman.dev>
2021-09-12 18:46:36 -0700

    tmux config for colors

--- shell/functions.sh
+++ shell/functions.sh
@@ -194,3 +194,19 @@ git_preview() {
 }
 
 
+# Displays color gradient, useful for confirming TERM color settings
+terminal_gradient() {
+  awk 'BEGIN{
+    s="/\\/\\/\\/\\/\\"; s=s s s s s s s s;
+    for (colnum = 0; colnum<77; colnum++) {
+        r = 255-(colnum*255/76);
+        g = (colnum*510/76);
+        b = (colnum*255/76);
+        if (g>255) g = 510-g;
+        printf "\033[48;2;%d;%d;%dm", r,g,b;
+        printf "\033[38;2;%d;%d;%dm", 255-r,255-g,255-b;
+        printf "%s\033[0m", substr(s,colnum+1,1);
+    }
+    printf "\n";
+  }'
+}
