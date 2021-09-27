ba0e7429cc34fc979082f4042cc17bfe17fffbf0
Matt Phillips <code@mattman.dev>
2021-09-27 10:50:23 -0700

    zsh updates for mac m1

--- shell/zprofile.sh
+++ shell/zprofile.sh
@@ -4,10 +4,10 @@
 zprezto_file=${ZDOTDIR:-$HOME}/.zprezto/runcoms/zprofile
 source "$zprezto_file"
 
-
 export EDITOR=vim
 export VISUAL=vim
 
+export PLATFORM="$(uname -s | tr A-Z a-z)"
 export SCRIPTS="$HOME/bin"
 export WORKSPACE="$HOME/workspace"
 export NOTES="$HOME/Notes"
@@ -15,10 +15,32 @@ export DOCUMENTS="$HOME/Documents"
 export PCLOUD="$HOME/pCloud Drive"
 
 
+# MacOS
+if [[ "$PLATFORM" == "darwin" ]]; then
+  # homebrew on M1 is in a different directory
+  if [[ -d /opt/homebrew ]]; then
+    path=(
+      /opt/homebrew/bin
+      /opt/homebrew/sbin
+      $path
+    )
+  fi
+
+  # python user bin directory is nonstandard on MacOS
+  # prezto seems to handle this. see 'python' in zpreztorc
+#  if [[ $(command -v python-user-bin) ]]; then
+#    path=(
+#      $path
+#      $(python-user-bin)
+#    )
+#  fi
+fi
+
 export GOPATH="$XDG_DATA_HOME/go"
-if [[ -n "$(command -v go)" ]]; then
+if [[ $(command -v go) ]]; then
   export GOROOT=$(go env GOROOT)
   #export GOBIN="$XDG_LOCAL_BIN"
+  export GOBIN="$GOPATH/bin"
 fi
 
 # enable history in iEx
@@ -28,12 +50,14 @@ export ERL_AFLAGS="-kernel shell_history enabled"
 path=(
   $HOME/bin
   $XDG_LOCAL_BIN
-  /opt/homebrew/bin
   $path
-  $GOPATH/bin
+  $GOBIN
 )
 
+
 cdpath=(
+  $DOCUMENTS
+  $NOTES
   $WORKSPACE
   $WORKSPACE/websites
   $GOPATH/src/github.com/imattman
