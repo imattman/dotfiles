96b9005884136531203d858de667b56cc65ea57f
Matt Phillips <code@mattman.dev>
2021-10-12 21:45:52 -0700

    separated MS fonts as optional install on linux

--- linux/install-fonts.sh
+++ linux/install-fonts.sh
@@ -5,12 +5,54 @@ THIS_DIR=$(dirname "$0")
 BASE_DIR=$(cd "$THIS_DIR" && pwd)
 
 
-sudo apt install -y \
-  fonts-firacode \
-  fonts-hack \
-  fonts-inconsolata \
-  fonts-crosextra-carlito \
-  fonts-crosextra-caladea \
-  ttf-mscorefonts-installer
+
+usage() {
+  cat<<EOU
+  Usage: $THIS_SCRIPT [OPTIONS]
+
+  Install fonts on a debian/ubuntu linux system.
+
+  OPTIONS:
+     -h:  Show this message
+     -m:  Install Microsoft fonts (presents EULA)
+
+EOU
+}
+
+
+ms_fonts() {
+  # keep this as a separate manual install option 
+  # because of modal EULA
+  echo "Installing MS fonts..."
+
+  sudo apt install -y \
+    ttf-mscorefonts-installer
+}
+
+
+std_fonts() {
+  echo "Installing fonts..."
+
+  sudo apt install -y \
+    fonts-firacode \
+    fonts-hack \
+    fonts-inconsolata \
+    fonts-crosextra-carlito \
+    fonts-crosextra-caladea
+}
+
+
+case "$1" in
+  -h)
+    usage
+    exit 1
+    ;;
+  -m)
+    ms_fonts
+    ;;
+  *)
+    std_fonts 
+    ;;
+esac
 
 
