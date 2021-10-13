60c80128b7797137506a434e535e9bc447ff0115
Matt Phillips <matt.phillips@gmail.com>
2021-10-13 11:45:43 -0700

    clean up linux install scripts

--- linux/install-openssh-server.sh
+++ linux/install-openssh-server.sh
@@ -5,10 +5,10 @@ THIS_DIR=$(dirname "$0")
 BASE_DIR=$(cd "$THIS_DIR" && pwd)
 
 sudo apt update
-sudo apt install openssh-server
+sudo apt install -y openssh-server
 
 # verify it started
-sudo systemctl status ssh
+sudo systemctl status --no-pager ssh
 
 echo "Be sure to check if firewall needs to be updated"
 echo "  sudo ufw allow ssh"
