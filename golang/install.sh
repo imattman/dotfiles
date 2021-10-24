951ffa36d3a35d56bff068cc596874fee49adba0
Matt Phillips <matt.phillips@gmail.com>
2021-10-24 14:26:05 -0700

    wip: updated golang pkgs to install

--- golang/install.sh
+++ golang/install.sh
@@ -16,34 +16,41 @@ BASE_DIR=$(cd "$THIS_DIR" && pwd)
 PKG_FILE='golang-pkg-list.txt'
 PKG_FILE="$THIS_DIR/$PKG_FILE"
 
-GOMOD_FILE='golang-gomod-list.txt'
-GOMOD_FILE="$THIS_DIR/$GOMOD_FILE"
+APP_FILE='golang-app-list.txt'
+APP_FILE="$THIS_DIR/$APP_FILE"
 
 # allow package file to be specified as argument
 PKG_FILE="${1:-$PKG_FILE}"
 
-GOMOD_PREFIX='env GO111MODULE=on'
-CMD_PREFIX=''
+#GOMOD_PREFIX='env GO111MODULE=on'
+GOMOD_PREFIX=''
+CMD_PREFIX='echo '
+
+go_get() {
+  install_from_file "get" "$1"
+}
+
+
+go_install() {
+  install_from_file "install" "$1"
+}
 
 
 install_from_file() {
-  local= pkg_file="$1"
+  local fetch_type="$1"
+  local pkg_file="$2"
 
-  if [[ $(echo $pkg_file | grep -- '-gomod-') ]] ; then
-    echo Installing Go module packages from $pkg_file
-    CMD_PREFIX="$CMD_PREFIX $GOMOD_PREFIX"
-  else
-    echo Installing classic packages from $pkg_file
-    if [[ -z "$GOPATH" ]] ; then
-      echo '$GOPATH is not set'
-      exit 1
-    fi
+  if [[ -z "$GOPATH" ]] ; then
+    echo '$GOPATH is not set'
+    exit 1
   fi
+
+  echo "[go $fetch_type] processing entries from $pkg_file"
 
   sed -e 's/\s*#.*//' -e '/^\s*$/d' < "$pkg_file" | \
   while read -r pkg; do
-    echo $CMD_PREFIX go install -v $pkg
-    $CMD_PREFIX go install -v $pkg
+    echo $CMD_PREFIX go $fetch_type -v $pkg
+    $CMD_PREFIX go $fetch_type -v $pkg
     echo
   done
 }
@@ -55,10 +62,10 @@ if [[ ! "$(command -v go)" ]] ; then
   exit 1
 fi
 
-install_from_file $PKG_FILE
+go_get $PKG_FILE
 
 if [[ $# -eq 0 ]] ; then
-  install_from_file $GOMOD_FILE
+  go_install $APP_FILE
 fi
 
 exit 0
