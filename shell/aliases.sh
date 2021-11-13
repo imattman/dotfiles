29c039928b0ff3c06f883812e07c6cb7fae54caf
Matt Phillips <code@mattman.dev>
2021-11-12 16:36:46 -0800

    fix repo update alias to match script rename

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -24,7 +24,7 @@ alias tstamp-num="date '+%Y%m%d.%H%M%S'"
 alias busy='look_busy'
 
 # update local git repos
-alias repoup='local-repos update'
+alias repoup='repos-local update'
 
 # homebrew
 alias brewup="brew update && brew upgrade && brew cleanup && brew doctor"
