8f6923b33aaf695320c9568f5622335ff4244004
Matt Phillips <matt.phillips@gmail.com>
2021-11-17 11:27:04 -0800

    experimenting with tmux always on

--- shell/zprofile.sh
+++ shell/zprofile.sh
@@ -1,5 +1,14 @@
 # mattman  .dotfiles/zsh/zprofile.sh
 
+# auto launch tmux
+AUTO_TMUX="${AUTO_TMUX:-on}"
+
+if [[ "$AUTO_TMUX" == 'on' ]] && [[ $(command -v tmux) ]] && [[ -n "$PS1" ]] && [[ -z "$TMUX" ]] && [[ ! "$TERM" =~ "tmux" ]] && [[ ! "$TERM" =~ "screen" ]]; then
+  #tmux && exit; # detach causes exit
+  exec tmux
+  #tmux attach || exec tmux new-session
+fi
+
 # init zprezto:zprofile
 source "${ZDOTDIR:-$HOME}/.zprezto/runcoms/zprofile"
 
