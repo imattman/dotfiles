00bb61395609657e18e7b761d8891893c3794d0e
Matt Phillips <code@mattman.dev>
2021-11-20 08:56:41 -0800

    move httpd go code to sample source file under 'golang'

--- golang/http_server.go
+++ golang/http_server.go
@@ -1,18 +1,3 @@
-#!/usr/bin/env bash
-set -e 
-
-base_dir="$(cd $(dirname $0) && pwd)"
-timestamp=$(date '+%Y%m%d-%H%M%S')
-
-function start_go_server() {
-  full_src="/tmp/go-server-${timestamp}.go"
-  echo "Go server source: $full_src"
-  echo "To re-run:"
-  echo
-  echo "  go run $full_src [docroot]"
-  echo
-
-  cat > "$full_src" << END_GO_SERVER
 // A basic Go HTTP file server with some additional logic for logging requests
 package main
 
@@ -23,6 +8,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"time"
 )
@@ -50,9 +36,11 @@ func main() {
 		IdleTimeout:  10 * time.Second,
 	}
 
-	done := make(chan bool)
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	var serverWg sync.WaitGroup
+	serverWg.Add(1)
 
 	go func() {
 		// signal handler triggers graceful shutdown
@@ -67,16 +55,17 @@ func main() {
 		if err := server.Shutdown(ctx); err != nil {
 			logger.Fatalf("Could not shut down server gracefully: %v\n", err)
 		}
-		close(done)
+
+		serverWg.Done()
 	}()
 
 	logger.Printf("Starting server on %s \n", addr)
-	if err := server.ListenAndServe(); err != nil {
-		logger.Fatal(err)
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		logger.Printf("Server shutdown with error: %v\n", err)
 	}
 
 	// let shutdown complete
-	<-done
+	serverWg.Wait()
 }
 
 func logRequests(logger *log.Logger, hdlr http.Handler) http.Handler {
@@ -85,20 +74,3 @@ func logRequests(logger *log.Logger, hdlr http.Handler) http.Handler {
 		logger.Println(r.Method, r.URL.Path, r.RemoteAddr)
 	})
 }
-END_GO_SERVER
-
-  go run "$full_src" "$@"
-}
-
-
-#if go version >/dev/null 2>&1 ; then
-if [[ $(go version 2>/dev/null) ]] ; then
-  start_go_server "$@"
-elif [[ $(python3 -V 2>/dev/null) ]] ; then
-  echo "Starting python3 http server"
-  python3 -m http.server "$@"
-else
-  echo "Attempting to start python2 http server"
-  python -m SimpleHTTPServer
-fi
-
