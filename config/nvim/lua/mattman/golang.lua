8bef4aefb670e0360fb21c7fef907ba8438e65f2
Matt Phillips <code@mattman.dev>
2022-02-07 10:58:45 -0800

    neovim golang imports helper function, per docs

--- config/nvim/lua/mattman/golang.lua
+++ config/nvim/lua/mattman/golang.lua
@@ -38,3 +38,37 @@ require'lspconfig'.gopls.setup{
   end,
 }
 
+-- copied from gopls docs
+function goimports(timeoutms)
+  local context = { source = { organizeImports = true } }
+  vim.validate { context = { context, "t", true } }
+
+  local params = vim.lsp.util.make_range_params()
+  params.context = context
+
+  local method = "textDocument/codeAction"
+  local resp = vim.lsp.buf_request_sync(0, method, params, timeoutms)
+  if resp and resp[1] then
+    local result = resp[1].result
+    if result and result[1] then
+      local edit = result[1].edit
+      vim.lsp.util.apply_workspace_edit(edit)
+    end
+  end
+
+  vim.lsp.buf.formatting()
+end
+
+vim.cmd [[
+  autocmd BufWritePre *.go lua goimports(1000)
+]]
+
+-- vim.cmd [[
+--   augroup _go
+--     autocmd!
+--     au! BufWritePre <buffer>
+--     autocmd BufWritePre *.go lua goimports(1000)
+--   augroup end
+-- ]]
+    -- autocmd BufWritePre <buffer> :lua vim.lsp.buf.formatting_sync(nil, 500)
+
