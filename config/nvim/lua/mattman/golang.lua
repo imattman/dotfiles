2625b6cc9d1060bee306c269e7a8e6f771891e71
Matt Phillips <code@mattman.dev>
2022-02-07 11:06:27 -0800

    updated Go lsp config format/organize imports
    Updated per example solution provided on github issue
    - https://github.com/neovim/nvim-lspconfig/issues/115

--- config/nvim/lua/mattman/golang.lua
+++ config/nvim/lua/mattman/golang.lua
@@ -39,28 +39,51 @@ require'lspconfig'.gopls.setup{
 }
 
 -- copied from gopls docs
-function goimports(timeoutms)
-  local context = { source = { organizeImports = true } }
-  vim.validate { context = { context, "t", true } }
+-- function goimports(timeoutms)
+--   local context = { source = { organizeImports = true } }
+--   vim.validate { context = { context, "t", true } }
+-- 
+--   local params = vim.lsp.util.make_range_params()
+--   params.context = context
+-- 
+--   local method = "textDocument/codeAction"
+--   local resp = vim.lsp.buf_request_sync(0, method, params, timeoutms)
+--   if resp and resp[1] then
+--     local result = resp[1].result
+--     if result and result[1] then
+--       local edit = result[1].edit
+--       vim.lsp.util.apply_workspace_edit(edit)
+--     end
+--   end
+-- 
+--   vim.lsp.buf.formatting()
+-- end
 
+
+-- following example solution from github issue:
+--   https://github.com/neovim/nvim-lspconfig/issues/115
+function goimports(wait_ms)
   local params = vim.lsp.util.make_range_params()
-  params.context = context
-
-  local method = "textDocument/codeAction"
-  local resp = vim.lsp.buf_request_sync(0, method, params, timeoutms)
-  if resp and resp[1] then
-    local result = resp[1].result
-    if result and result[1] then
-      local edit = result[1].edit
-      vim.lsp.util.apply_workspace_edit(edit)
+  params.context = {only = {"source.organizeImports"}}
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+  for _, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        -- note: text encoding param is required
+        vim.lsp.util.apply_workspace_edit(r.edit, "utf-16")
+      else
+        vim.lsp.buf.execute_command(r.command)
+      end
     end
   end
-
-  vim.lsp.buf.formatting()
 end
 
 vim.cmd [[
-  autocmd BufWritePre *.go lua goimports(1000)
+augroup GO_LSP
+	autocmd!
+	autocmd BufWritePre *.go :silent! lua vim.lsp.buf.formatting()
+	autocmd BufWritePre *.go :silent! lua goimports(3000)
+augroup END
 ]]
 
 -- vim.cmd [[
