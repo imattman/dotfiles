49e191ea4d00fd2d5d043985cd41c7001dc67c00
Matt Phillips <code@mattman.dev>
2022-04-10 09:32:56 -0700

    fixed usage indentation

--- linux/install-neovim-from-src.sh
+++ linux/install-neovim-from-src.sh
@@ -12,23 +12,26 @@ CLONE_DIR="$WORKSPACE/neovim"
 
 
 usage() {
-  cat<<EOU
-  Usage: $THIS_SCRIPT [subcommand]
+  cat<<-EOU
+	Usage: $THIS_SCRIPT [subcommand]
+	
+	Installs *neovim* from source on a fresh linux system.
+	
+	The default is to execute all of the following subcommands in order:
+	
+	  deps          installs compile dependencies
+	  clone         clones source git repo to \$WORKSPACE
+	  build         (re)builds default 'make' target
+	  install       installs 'nvim' binary via neovim make target
+	
+	If an existing workspace is detected, it will be updated.
+	
+	  update        updates git repo to latest
 
-  Installs *neovim* from source on a fresh linux (Ubuntu) system.
+	Maintenance commands:
 
-  The default is to execute all of the following subcommands in order:
-
-    deps          installs compile dependencies using 'apt install'
-    clone         clones neovim git repo to \$WORKSPACE
-    build         (re)builds default 'make' target
-    install       installs 'nvim' binary via neovim make target
-    clean_cache   removes local nvim cache directories
-    latest        clones, updates repo, builds, and installs latest from source
-
-  If an existing workspace is detected, it will be updated.
-
-    update        updates git repo to latest
+	  clean_cache   removes local nvim cache directories
+	  latest        clones, updates repo, builds, and installs latest from source
 
 EOU
 }
@@ -137,6 +140,7 @@ if [[ $# -eq 0 ]]; then
   clone
   build
   install
+  details
 else
   for arg in "$@"; do
     case "$arg" in
