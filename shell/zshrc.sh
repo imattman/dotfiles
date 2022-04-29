039e0b9b62e88b2af12abdd38382a27ae531bc97
Matt Phillips <code@mattman.dev>
2022-04-29 11:46:02 -0700

    reducing pause before exit after tmux session

--- shell/zshrc.sh
+++ shell/zshrc.sh
@@ -50,7 +50,7 @@ autoload -Uz compinit && compinit
 AUTO_TMUX="${AUTO_TMUX:-off}"
 
 if [[ "$AUTO_TMUX" == 'on' ]] && [[ $(command -v tmux) ]] && [[ -n "$PS1" ]] && [[ -z "$TMUX" ]] && [[ ! "$TERM" =~ "tmux" ]] && [[ ! "$TERM" =~ "screen" ]]; then
-  tmux && echo "sleeping 3 before exit..." && sleep 3 && exit
+  tmux && echo "pausing before exit..." && sleep 1.5 && exit
 fi
 
 
