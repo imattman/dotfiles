1a5ee72efc4299fd20bba9eefa11fc744b4c044e
Matt Phillips <code@mattman.dev>
2022-06-21 13:27:41 -0700

    updated emacs config with 'melpa' repo and 'use' utility package

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -7,21 +7,37 @@
 ;(load-theme 'wombat t)
 
 (setq inhibit-startup-message t)  ;; disable start up screen
-(menu-bar-mode -1)                ;;
 (tool-bar-mode -1)                ;; 
-(toggle-scroll-bar -1)            ;; 
+;(toggle-scroll-bar -1)
+
+;(setq initial-scratch-message "") ;; hide scratch buffer help
+
+;; note the visual warning causes problems on OS X
+(setq ring-bell-function 'ignore) ;; disable audio/visual warning
 
 (global-linum-mode t)             ;; enable line numbers
 (setq default-tab-width 4)        ;; use 4 because 8 is too wide
 
-(setq initial-scratch-message "") ;; hide scratch buffer help
-
-
-;; note the visual warning causes problems on OS X
-(setq ring-bell-function 'ignore) ;; disable audio/visual warning
 
 ;(print (font-family-list))
 (set-face-attribute 'default nil
   :family "Hack Nerd Font" :height 160 :weight 'normal)
 ;  :family "FiraCode Nerd Font" :height 160 :weight 'normal)
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; bootstrap 'use-package'
+(unless (package-installed-p 'use-package)
+        (package-refresh-contents)
+        (package-install 'use-package))
+
+(use-package which-key
+  :ensure t)
+
+(use-package try
+  :ensure t)
 
