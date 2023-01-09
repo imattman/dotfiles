bce99d5e507de83d2a346317f6ee364675c58c5c
Matt Phillips <code@mattman.dev>
2023-01-09 12:23:06 -0800

    disabled default setting of GIT_EDITOR

--- shell/local.sh
+++ shell/local.sh
@@ -3,9 +3,10 @@
 # This file serves as home for local configuration not appropriate
 # or applicable for general source control
 
-export AUTO_TMUX="off"
-#export EDITOR="nvim"
-export VISUAL="nvim"
+export AUTO_TMUX="on"
+export EDITOR="nvim"
+#export VISUAL="nvim"
+#export GIT_EDITOR=vimc
 
 export FOO_API_TOKEN="xxxxxxx"
 
