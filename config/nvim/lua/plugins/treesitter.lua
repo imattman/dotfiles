a9452a70854cd942206048eb77ceb418030bafe8
Matt Phillips <code@mattman.dev>
2024-10-21 11:02:08 -0700

    neovim: disable treesitter with HTML (temporary)

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -20,8 +20,27 @@ return {
       },
       -- auto_install = true,
       sync_install = false,
-      highlight = { enable = true },
+      highlight = {
+        enable = true,
+        disable = function(lang, buf)
+          if lang == "html" then
+            return true
+          end
+
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            vim.notify(
+              "File larger than 100KB; treesitter disabled for performance",
+              vim.log.levels.WARN,
+              {title = "Treesitter"}
+            )
+            return true
+          end
+        end
+      },
       indent = { enable = true },
     })
   end
 }
+
